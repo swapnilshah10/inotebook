ccb77c4366676c0417e95d932290f661974afa7e
Swapnil <swapnilshah102002@gmail.com>
2022-05-04 20:13:42 +0530

    additional files and changes for Heroku

--- inotebook/settings.py
+++ inotebook/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,4 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    ]
+    ]   
+django_heroku.settings(locals())
