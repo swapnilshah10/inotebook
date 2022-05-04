6aeff35f17d1f4aea02b758ccf19f6e5009a3b3e
Swapnil <swapnilshah102002@gmail.com>
2022-05-04 22:04:02 +0530

    additional files and changes for Heroku

--- notes/models.py
+++ notes/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-q =  User.objects.get(id = 1)
 class Notes(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE ,default= q)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE ,default= 1)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     tags = models.TextField(null=True, blank=True)
