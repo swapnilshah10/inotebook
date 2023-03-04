36f86881decfb437b5f96420753391f542ad2809
Swapnil <swapnilshah102002@gmail.com>
2023-03-04 16:46:40 +0530

    updated basic ui

--- src/components/Note2.js
+++ src/components/Note2.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-// import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -8,7 +7,6 @@ import Typography from "@mui/material/Typography";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import DeleteIcon from '@mui/icons-material/Delete';
-
 
 export default function BasicCard(props) {
   let url = `https://inotebook123.herokuapp.com/delete-notes/${props.data.id}/`;
@@ -31,26 +29,35 @@ export default function BasicCard(props) {
   };
 
   return (
-    <>
-    <Card sx={{   backgroundColor:"#00FA9A" }}>
+    <Card sx={{ 
+        width: 350, 
+        height: 250,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        backgroundColor: "#E0FFFF",
+        boxShadow: "4px 4px 10px rgba(0, 0, 0, 0.3)",
+        borderRadius: "15px",
+        overflow: "hidden",
+        position: "relative"
+      }}>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: "bold", color: "#004D40" }}>
           {props.data.name}
         </Typography>
-        <Typography variant="h5" component="div">
+        <Typography variant="body1" sx={{ mb: 1.5, color: "#004D40" }}>
           {props.data.description}
         </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        <Typography variant="body2" sx={{ color: "#004D40" }}>
           {props.data.tags}
         </Typography>
       </CardContent>
-      <CardActions>
+      <CardActions sx={{ position: "absolute", bottom: 0, right: 0 }}>
         <Link to={`/updatenote/${props.data.id}/`}>
-          <Button variant="contained" size="small">Update</Button>
+          <Button variant="contained" size="small" sx={{ borderRadius: "0 0 0 15px", backgroundColor: "#8BC34A", color: "white", textDecoration :false ,"&:hover": { backgroundColor: "#7CB342" , textDecoration :"none" } }}>Update</Button>
         </Link>
-        <Button  sx={{ mb: 0.5  , color : "grey"}} onClick={delData} size="small"><DeleteIcon /></Button>
+        <Button onClick={delData} size="small" sx={{ borderRadius: "0 0 15px 0", color: "#BDBDBD", "&:hover": { color: "#616161" } }}><DeleteIcon /></Button>
       </CardActions>
     </Card>
-    </>
   );
 }
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -6,12 +6,11 @@ import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import { Navigate } from "react-router-dom";
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
 
 // let url = "https://inotebook123.herokuapp.com/get-notes/";
 let url = "http://127.0.0.1:8000/get-notes/";
-
 
 function Notes(props) {
   const token = localStorage.getItem("token");
@@ -28,7 +27,7 @@ function Notes(props) {
         setDats(res.data);
       })
       .catch((err) => console.log(err));
-    };
+  };
 
   useEffect(() => {
     getData();
@@ -43,65 +42,81 @@ function Notes(props) {
   if (isLoggedout) {
     return <Navigate to="/" />;
   }
- 
-  var style={backgroundImage: "url(/background.jpg)",
-  backgroundSize: "cover",
-  backgroundRepeat: "repeat-y"}
 
-  if(data.length < 9 && window.innerWidth > 900){
-    style = {backgroundImage: "url(/background.jpg)",
+  const backgroundImg = "/background.jpg";
+  const minHeight = data.length < 9 && window.innerWidth > 900 ? "100vh" : "auto";
+  const backgroundStyle = {
+    backgroundImage: `url(${backgroundImg})`,
     backgroundSize: "cover",
-    backgroundRepeat: "repeat-y",height: '100vh'}
-  }
+    backgroundRepeat: "repeat-y",
+    minHeight: minHeight,
+  };
 
   return (
-    <div style={style}>
+    <div style={backgroundStyle}>
       <CssBaseline />
       <Box sx={{ flexGrow: 1 }}>
-      <Box
-        sx={{
-          "& button": { m: 1 },
-          alignitem: "center",  
-          justifycontent: "center",
-        }}
-      >
-
-        <br></br>
-        <Button variant="text" onClick={logout} style={{ float: "right" , height: "10px" }}>
-          Logout
-        </Button>
-        <br></br>
-       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-       
-          {data &&
-            data.map((element) => {
-              if (!data) return null;
-              else {
-                return (
-                  <Grid item xs sm={4} md={3} key={element.id}>
-                    <li
-                      className="list-group-item"
-                      style={{
-                        justifycontent: "center",
-                        margin: "10px",
-                        padding: "0px",
-                      }}
-                    >
-                      <Note2
-                        data={element}
-                        getdata={getData}
-                        token={props.token}
-                      />
-                    </li>
-                  </Grid>
-                );
-              }
-            })}
-      </Grid>
-        <Link to="/createnote">
-          <Button variant="contained" sx={{ position: 'fixed', bottom: 0, right: "0%" ,alignItems : "center"}} >Make a note</Button>
-        </Link>
-      </Box>
+        <Box
+          sx={{
+            "& button": { m: 1 },
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <br />
+          <Button
+            variant="text"
+            onClick={logout}
+            style={{ float: "right", height: "10px" }}
+          >
+            Logout
+          </Button>
+          <br />
+          <Grid
+            container
+            spacing={{ xs: 2, md: 3 }}
+            columns={{ xs: 4, sm: 8, md: 12 }}
+            sx={{ marginTop: "40px", marginBottom: "40px" }}
+          >
+            {data &&
+              data.map((element) => {
+                if (!data) return null;
+                else {
+                  return (
+                    <Grid item xs sm={4} md={3} key={element.id}>
+                      <li
+                        className="list-group-item"
+                        style={{
+                          justifyContent: "center",
+                          margin: "10px",
+                          padding: "0px",
+                        }}
+                      >
+                        <Note2
+                          data={element}
+                          getdata={getData}
+                          token={props.token}
+                        />
+                      </li>
+                    </Grid>
+                  );
+                }
+              })}
+          </Grid>
+          <Link to="/createnote">
+            <Button
+              variant="contained"
+              sx={{
+                position: "fixed",
+                bottom: 0,
+                right: "0%",
+                alignItems: "center",
+              }}
+            >
+              Make a note
+            </Button>
+          </Link>
+        </Box>
       </Box>
     </div>
   );
